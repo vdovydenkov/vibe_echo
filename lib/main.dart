1db6798aa97f642200a85ed3c3b6bd96a4109e72
VDovydenkov <dov@mlbox.ru>
2025-10-23 11:57:24 +0300

    add debug messages.

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,32 @@ const appTitle = 'Vibe Echo';
 void vibrate(int vibroPreset) {
   switch (vibroPreset) {
     case 1:
-      Vibration.vibrate(duration: 500); // вибрация 0.5 секунды
+      Vibration.vibrate(duration: 1000); // вибрация одну секунду: всегда для теста
+      debugPrint('duration: 1000');
       break;
     case 2:
-      Vibration.vibrate(duration: 1000, amplitude: 128); // средняя сила вибрации
+      Vibration.vibrate(duration: 2000, amplitude: 96); // средняя сила вибрации
+      debugPrint('duration: 2000, amplitude: 96');
       break;
     case 3:
-      Vibration.vibrate(pattern: [300, 400, 300, 600]);
+      Vibration.vibrate(pattern: [200, 300, 200, 300, 200, 300]);
+      debugPrint('pattern: [200, 300, 200, 300, 200, 300]');
       break;
     case 4:
       Vibration.vibrate(
-        pattern: [200, 400, 200, 600, 200, 800],
-        intensities: [50, 128, 200],
+        pattern: [200, 400, 200, 600, 200, 800, 200, 1000],
+        intensities: [50, 128, 200, 250],
       );
+      debugPrint('pattern: [200, 400, 200, 600, 200, 800, 200, 1000],\nintensities: [50, 128, 200, 250]');
       break;
     case 5:
       // Готовые паттерны singleShortBuzz, doubleBuzz, heartbeatVibration, emergencyAlert
-      Vibration.vibrate(preset: VibrationPreset.doubleBuzz);
+      Vibration.vibrate(preset: VibrationPreset.emergencyAlert);
+      debugPrint('preset: VibrationPreset.emergencyAlert');
       break;
     default:
-      Vibration.vibrate(duration: 2000);
+      Vibration.vibrate(duration: 5000);
+      debugPrint('Default case: duration: 5000');
   }
 }
 
