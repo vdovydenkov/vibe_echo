fcd77a216a06633b71df7b88d5f2715e40beba6c
VDovydenkov <dov@mlbox.ru>
2025-11-13 19:05:14 +0300

    style: reformat some lines

--- lib/services/haptics/haptic_factory.dart
+++ lib/services/haptics/haptic_factory.dart
@@ -4,9 +4,10 @@ import 'package:vibe_echo/services/haptics/haptic_interface.dart';
 import 'package:vibe_echo/services/haptics/haptic_simple.dart';
 import 'package:vibe_echo/services/haptics/haptic_advanced.dart';
 
-final hapticMode = String.fromEnvironment('HAPTIC_MODE',
-    defaultValue: 'advanced')
-    .toUpperCase();
+final hapticMode = String.fromEnvironment(
+  'HAPTIC_MODE',
+  defaultValue: VibeMode.advanced.name,
+).toUpperCase();
 
 Future<HapticEngine> createHapticEngine() async {
   if (hapticMode == 'ADVANCED') {
